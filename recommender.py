d467bebd6a3611acecf149f60b78fbff94a37702
Aryan <singh9818aryan@gmail.com>
2026-05-15 18:34:30 +0530

    Optimized deployment memory usage

--- recommender.py
+++ recommender.py
@@ -1,7 +1,9 @@
 import json
 import numpy as np
 import faiss
-from sentence_transformers import SentenceTransformer('all-MiniLM-L6-v2')
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 
 # -----------------------------
